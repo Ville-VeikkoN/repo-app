f1436f4a38f26ac57b2c9324bcad5e3787fa8544
VeeVee <ville-veikko.nieminen@tuni.fi>
2019-12-08 12:14:30 +0200

    Change: modal information and deleted unused imports

--- components/commit/CommitModal.js
+++ components/commit/CommitModal.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, TouchableOpacity, FlatList, Button, Image } from 'react-native';
-import { Provider } from "react-redux";
+import React from 'react';
+import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { Card } from 'react-native-elements';
 import Modal from 'react-native-modal';
 import commitModalStyle from './CommitModal.style'
-
+import styles from '../../Styles';
+import { getParsedDate } from '../../helpers/dateHelper'
 
 export default class CommitModal extends React.Component {
 
@@ -24,18 +24,16 @@ export default class CommitModal extends React.Component {
           backdropTransitionOutTiming={0}
           onBackdropPress={() => this.props.handleClose()}
           onBackButtonPress={() => this.props.handleClose()}>
-
-          <Card title={commit.sha}>
+          <Card containerStyle={styles.cardContainer} title={getParsedDate(commit.commit.author.date)}>
             <View style={commitModalStyle.modalcontent}>
-            <Text style={{fontSize:13, borderBottomColor:'gray', borderBottomWidth:1}}>Author</Text>
               {commit.author != null && 
                   <Image
                     style={commitModalStyle.image}
                     source={{uri: commit.author.avatar_url}}
                   />
               }
-              <Text style={{fontSize:20, marginBottom: 20}}>{commit.commit.author.name}</Text>
-              <Text style={{fontSize:13, borderBottomColor:'gray', borderBottomWidth:1}}>Commit Message</Text>
+              <Text style={styles.username}>{commit.commit.author.name}</Text>
+              <Text style={{fontSize:13, borderBottomColor:'gray', borderBottomWidth:1}}>Message</Text>
               <Text style={{fontSize:20, textAlign:'center'}}>{commit.commit.message}</Text>
               <TouchableOpacity onPress={() => this.props.handleClose()}>
                 <Text style={commitModalStyle.closeText}>close</Text>
